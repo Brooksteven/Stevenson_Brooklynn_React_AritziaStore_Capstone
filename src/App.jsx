3efc1a96aeab1b127ba7ef151e531e25479a01dd
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-05-12 15:52:09 -0400

    initial files and setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        {/* here is where we make our individual routes */}
+        <Route path='/' element={<HomePage />}/>
+        {/* add the rest of the pages you want here */}
+      </Routes>
+    </>
+  )
+}
